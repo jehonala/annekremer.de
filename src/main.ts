7177809a496ee53693743472dc8f8e983905ae11
Tobias Kremer <tobias.kremer@dg-i.net>
2025-12-21 13:14:04 +0100

    add ambient declarations for fontsource

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import "@fontsource-variable/inter";
 import { insertRandomFunfact } from "./funfacts.ts";
 
 const element = document.getElementById("quote");
-
 if (element) {
   insertRandomFunfact(element);
 }
